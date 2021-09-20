4df7cdd2f2eed7d7274e075a65f3cb93b71437cd
Crowdbotics <team@crowdbotics.com>
2021-09-20 10:09:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mayusapp_30697Identifier",
+  url: "https://mayusapp_30697Identifier.botics.co",
+  api: "https://mayusapp_30697Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
